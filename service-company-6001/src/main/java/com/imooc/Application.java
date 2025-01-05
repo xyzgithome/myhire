9c2ca6a39e0933b61b3dfc0c18bbe12040417664
xyzgithome <2584118953@qq.com>
2025-01-05 22:23:03 +0800

    user、company注册到nacos

--- service-company-6001/src/main/java/com/imooc/Application.java
+++ service-company-6001/src/main/java/com/imooc/Application.java
@@ -6,8 +6,10 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
--- service-user-7001/src/main/java/com/imooc/Application.java
+++ service-user-7001/src/main/java/com/imooc/Application.java
@@ -1,15 +1,13 @@
-
-//	此资源由 58学课资源站 收集整理
-//	想要获取完整课件资料 请访问：58xueke.com
-//	百万资源 畅享学习
 package com.imooc;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.imooc.mapper")
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
